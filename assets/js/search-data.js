75cba9f074af78f8b33662ba08769f8bd77f8ed2
papr1ka2 <gyejinlee@gatech.edu>
2026-05-08 20:54:58 +0000

    Deploying to gh-pages from @ papr1ka2/papr1ka2.github.io@a02a70773abeff49a061127a50fdbb614798c6b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "CV",
           description: "",
           section: "Navigation",
