7161f3f73356b2529405ee4eb8e36bd67225d830
papr1ka2 <gyejinlee@gatech.edu>
2026-05-08 21:54:55 +0000

    Deploying to gh-pages from @ papr1ka2/papr1ka2.github.io@bb0c158c2b5ce95e7439853f9e48437cfdfeeedd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,44 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
           title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{
+        },{id: "post-hello-world",
+        
+          title: "hello, world",
+        
+        description: "first post.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/2026/05/08/hello-world.html";
+          
+        },
+      },{id: "projects-darpa-39-s-aixcc-team-atlanta",
+          title: 'DARPA&amp;#39;s AIxCC Team Atlanta',
+          description: "Winning entry of DARPA&#39;s AI Cyber Challenge (AIxCC). ATLANTIS, an AI-driven cyber reasoning system.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/team_atlanta/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
