aeb85fdfa2d86eabef882d55e4128dcb23dfcc55
papr1ka2 <gyejinlee@gatech.edu>
2026-05-10 00:57:00 +0000

    Deploying to gh-pages from @ papr1ka2/papr1ka2.github.io@48bcf1e30dbedcf3d742a137016c6f4f371d54af 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,69 +42,8 @@ ninja.data = [{
           
         },
       },{id: "projects-darpa-39-s-aixcc-team-atlanta",
-          title: 'DARPA&amp;#39;s AIxCC Team Atlanta',
+          title: 'DARPA&#39;s AIxCC Team Atlanta',
           description: "Winning entry of DARPA&#39;s AI Cyber Challenge (AIxCC). ATLANTIS, an AI-driven cyber reasoning system.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/team_atlanta/";
-            },},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%67%79%65%6A%69%6E%6C%65%65@%67%61%74%65%63%68.%65%64%75", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/papr1ka2", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/gyejinlee", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-x',
-        title: 'X',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://twitter.com/be11pepper", "_blank");
-        },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+            },},];
